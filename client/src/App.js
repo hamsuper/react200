5c74e66a9d129b39d9abf8c57014511095208c62
함희영 <hhy@nhnsoft.com>
2022-06-15 16:22:12 +0900

    [20220615] 003 Component 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import R003_ImportConponent from './R003_ImportConponent';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
+      <R003_ImportConponent></R003_ImportConponent>
     </div>
   );
 }
