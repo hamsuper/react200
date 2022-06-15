8cd8223ae8fce99b908cf136a785903f6e75922e
함희영 <hhy@nhnsoft.com>
2022-06-16 08:10:19 +0900

    [20220616] 015 map() 함수 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import R014_ForEach from './R014_ForEach';
+import R015_Map from './R015_Map';
 
 function App() {
   return (
 		<div>
 			<h1>Start React 200!</h1>
 			<p>CSS 적용하기</p>
-			<R014_ForEach />
+			<R015_Map />
 		</div>
   );
 }
---
+++ client/src/R015_Map.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class R015_Map extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+  componentDidMount() { 
+    let Map_Arr = [3, 2, 8, 8];
+    let Map_newArr = Map_Arr.map(x => x);
+    console.log(`1. Map_newArr : [${Map_newArr}]`);
+
+    let Map_multiArr = Map_Arr.map(x => x * 2);
+    console.log(`2. Map_multiArr : [${Map_multiArr}]`);
+
+    let ObjArray = [
+      { key: 'react', value: '200' },
+      { key: '리액트', value: 'TwoHundred' },
+    ];
+    let Map_objArr = ObjArray.map((obj, index) => {
+      console.log(`${index + 3}. obj : ${JSON.stringify(obj)}`);  
+      var Obj = {};
+      Obj[obj.key] = obj.value;
+      return Obj;
+    })
+    console.log(`5. Map_objArr : ${JSON.stringify(Map_objArr)}`);
+  }
+  
+	render() {
+		return <div>R015_Map</div>;
+	}
+}
