a4558aabca304f11c1d87e469b794a3d1e292953
함희영 <hhy@nhnsoft.com>
2022-06-15 16:59:07 +0900

    [20220615] 007 생명주기 함수 componentDidMount() 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import R006_LifecycleEx from './R006_LifecycleEx';
+import LifecycleEx from './R007_LifecycleEx';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <R006_LifecycleEx prop_value='FromApp.js'></R006_LifecycleEx>
+      <LifecycleEx prop_value='FromApp.js'></LifecycleEx>
     </div>
   );
 }
