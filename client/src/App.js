da77e283b458261426e8bf5c28e5687905814042
함희영 <hhy@nhnsoft.com>
2022-06-16 08:51:19 +0900

    [20220617] 018 props 자료형 선언하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import R017_Props from './R017_Props';
+import R018_PropsDatatype from './R018_PropsDatatype';
 
 function App() {
   return (
 		<div>
 			<h1>Start React 200!</h1>
 			<p>CSS 적용하기</p>
-			<R017_Props props_val='this is props' />
+			<R018_PropsDatatype
+				String="react"
+				Number={200}
+				Boolean={1 == 1}
+				Array={[0, 1, 8]}
+				ObjectJson={{ react: '리액트', twohundred: '200' }}
+				Function={console.log('FunctionProps: function!')}
+			/>
 		</div>
   );
 }
