d6e307de1e5cfaa6055158efb49b8cf38d17209b
함희영 <hhy@nhnsoft.com>
2022-06-16 07:37:41 +0900

    [20220616] 013 화살표 함수 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import R012_ClassPrototype from './R012_Class&prototype';
+import R013_ArrowFunction from './R013_ArrowFunction';
 
 function App() {
   return (
 		<div>
 			<h1>Start React 200!</h1>
 			<p>CSS 적용하기</p>
-			<R012_ClassPrototype />
+			<R013_ArrowFunction />
 		</div>
   );
 }
