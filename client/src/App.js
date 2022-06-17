8a9709c62bdada58f4bebe08b56cd257e9a9bb2d
함희영 <hhy@nhnsoft.com>
2022-06-17 10:11:10 +0900

    [20220620] 030 함수형 컴포넌트 사용하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import R029_ShallowEqual from './R029_ShallowEqual';
+import R030_FunctionComponent from './R030_FunctionComponent';
 
 function App() {
   return (
 		<div>
 			<h1>Start React 200!</h1>
 			<p>CSS 적용하기</p>
-			<R029_ShallowEqual />
+			<R030_FunctionComponent contents='함수형 컴포넌트' />
 		</div>
   );
 }
---
+++ client/src/R030_FunctionComponent.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function R030_FunctionComponent(props) {
+  let {contents} = props;
+  return (
+    <div>{contents}</div>
+  )
+}
