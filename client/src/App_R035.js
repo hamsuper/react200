886d09aff1438a4de76bec4c2862ae222926f3d0
함희영 <hhy@nhnsoft.com>
2022-06-19 14:02:22 +0900

    app035 백업

---
+++ client/src/App_R035.js
@@ -0,0 +1,15 @@
+import './App.css';
+import R035_ReactstrapBadges from './R035_ReactstrapBadges';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+		<div>
+			<h1>Start React 200!</h1>
+			<p>CSS 적용하기</p>
+			<R035_ReactstrapBadges />
+		</div>
+  );
+}
+
+export default App;
