d04e75fa0306d7701de94c30c0c783466792d80e
함희영 <hhy@nhnsoft.com>
2022-06-20 15:45:44 +0900

    [20220622] 045 reactstrap Input Group 사용하기

---
+++ client/src/R045_ReactstrapInputGroup.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { InputGroup, InputGroupText, Input, Button } from 'reactstrap';
+
+export default class R045_ReactstrapInputGroup extends Component {
+  render() {
+    return (
+		<>
+			<InputGroup>
+				<Input placeholder="userId" />
+				<InputGroupText>@reactmail.com</InputGroupText>
+			</InputGroup>
+        <InputGroup>
+          <Button>버튼</Button>
+				<Input />
+			</InputGroup>
+		</>
+	);
+  }
+}
