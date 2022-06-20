2b493570ad70af64cb9f43b6e10357b3ebf1995b
함희영 <hhy@nhnsoft.com>
2022-06-20 15:23:18 +0900

    [20220622] 042 reactstrap Collapse 사용하기

---
+++ client/src/R042_ReactstrapCollapse.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap'
+
+export default class R042_ReactstrapCollapse extends Component {
+  render() {
+    return (
+      <div>
+        <Button color="warning" id="toggle">펼치기/접기</Button>
+        <UncontrolledCollapse toggler='#toggle'>
+          <Card>
+            <CardBody>
+              React 200
+            </CardBody>
+          </Card>
+        </UncontrolledCollapse>
+      </div>
+    )
+  }
+}
