5a5709bfe50a20f13cf48e794ae304322c4f9a5b
함희영 <hhy@nhnsoft.com>
2022-06-28 15:04:41 +0900

    [20220628] 078 redux 리듀서로 스토어 생성하기 - 78~80번의 실행결과는 80번에서 확인

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'; //redux 패키지에서 스토어 생성 함수 createStore()를 임포트해 사용할 수 있도록 한다.
+import reducers from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const store = createStore(reducers);
+// createStore 함수의 파라미터로 reducers 폴더 경로를 넘긴다.
+// reducers 폴더의 index.js에는 데이터 초깃값을 설정하고 데이터를 변경해주는 함수(리듀서)가 있다.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const listener = () => {
+  ReactDOM.render(
+    <App store={store} />, // sotre를 App 컴포넌트에 전달한다. render 함수를 listener 함수 내부에 위치시킨다. 
+    document.getElementById('root')
+  )
+}
+store.subscribe(listener); // store를 구독하면 store 데이터에 변화가 있을 때 listener 함수 내부의 render 함수를 실행하고 변경된 데이터를 렌더링한다.
+listener(); // render 함수를 Listener 함수로 감쌌기 때문에 초기 렌더링을 위해 수동으로 render 함수를 실행시켜준다. 
