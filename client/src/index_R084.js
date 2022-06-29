758f35f6e34e0319ec061e203732501049cb9aaa
함희영 <hhy@nhnsoft.com>
2022-06-29 17:52:32 +0900

    [20220629] 084 redux 미들웨어 사용하기

--- client/src/index_R084.js
+++ client/src/index_R084.js
@@ -6,30 +6,30 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 
-const CallMiddleware = store => nextMiddle => action => {
-  // 다중 컬링 구조 (스토어, 리듀서 호출, 액션)
+const CallMiddleware = (store) => (nextMiddle) => (action) => {
+	// 다중 컬링 구조 (스토어, 리듀서 호출, 액션)
 
-  console.log('1. reducer 실행 전');
-  console.log('2. action.type : ' + action.type + ', store str : ' + store.getState().data.str);
+	console.log('1. reducer 실행 전');
+	console.log('2. action.type : ' + action.type + ', store str : ' + store.getState().data.str);
 
-  let result = nextMiddle(action);
-  console.log('3. reducer 실행 후')
-  console.log('4. action.type : ' + action.type + ', store str : ' + store.getState().data.str);
+	let result = nextMiddle(action);
+	console.log('3. reducer 실행 후');
+	console.log('4. action.type : ' + action.type + ', store str : ' + store.getState().data.str);
 
-  return result;
-}
+	return result;
+};
 
 const store = createStore(reducers, applyMiddleware(CallMiddleware));
 console.log('store', store);
 
 const listener = () => {
-  ReactDOM.render(
+	ReactDOM.render(
 		<Provider store={store}>
 			<App indexProp="react" />
 		</Provider>,
 		document.getElementById('root')
-  );
-}
+	);
+};
 
 store.subscribe(listener);
 listener();
