776f6eb5745cae970644eef4fea644abfaf8cfae
함희영 <hhy@nhnsoft.com>
2022-06-30 13:50:44 +0900

    [20220630] 089 react-router-dom Route 사용하기

---
+++ client/src/components/R089_reactRouter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class R089_reactRouter extends Component {
+  render() {
+    return (
+		<>
+			<h1>path='/'</h1>
+			<h3>R089_reactRouter</h3>
+		</>
+	);
+  }
+}
---
+++ client/src/components/R089_reactRouter2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class R089_reactRouter2 extends Component {
+  render() {
+    return (
+		<>
+			<h1>path='/reactRouter2'</h1>
+			<h3>R089_reactRouter2</h3>
+		</>
+	);
+  }
+}
