4154aaf918387e5352c751b380666d96319a4225
함희영 <hhy@nhnsoft.com>
2022-07-01 15:54:27 +0900

    [20220701] 103 recharts로 BarChart 구현하기 (서울시 유동 인구 데이터)

---
+++ client/src/components/Floating_population/floatingPopulationBarChart.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+	{ 군구: '광진구', 유동인구수: 32760, 비유동인구수: 34000 },
+	{ 군구: '동대문구', 유동인구수: 30480, 비유동인구수: 56000 },
+	{ 군구: '마포구', 유동인구수: 27250, 비유동인구수: 23000 },
+	{ 군구: '구로구', 유동인구수: 49870, 비유동인구수: 67000 },
+	{ 군구: '강남구', 유동인구수: 51420, 비유동인구수: 55000 },
+];
+
+export default class floatingPopulationBarChart extends PureComponent {
+	static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
+
+	render() {
+		return (
+			<BarChart width={1000} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+				<CartesianGrid strokeDasharray="5 5" />
+				<XAxis dataKey="군구" />
+				<YAxis />
+        <Tooltip />
+        <Legend />
+				<Bar type="monotone" dataKey="유동인구수" fill="#8884d8" />
+			</BarChart>
+		);
+	}
+}
