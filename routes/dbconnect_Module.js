d12c4efcff5310884236fa64b9866f9e72d42bce
함희영 <hhy@nhnsoft.com>
2022-07-06 16:23:37 +0900

    [20020706] 124 NODE 조회 api 만들기 3 - mybatis 패키지 사용하기

--- routes/dbconnect_Module.js
+++ routes/dbconnect_Module.js
@@ -1,4 +1,7 @@
+var express = require('express');
+var router = express.Router();
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 
@@ -6,14 +9,35 @@ router.use(bodyParser.json());
 const connection = mysql.createConnection({
 	host: 'database-1.cftrphdao14s.us-west-1.rds.amazonaws.com',
 	port: '3306',
-	database: 'react200',
+	database: 'react',
 	user: 'admin',
 	password: 'Ghdrlfehd1!',
 });
 
 router.post('/', (req, res) => {
+	const mybatisMapper = require('mybatis-mapper');
+	var param = req.body;
+
+	//mybatis mapper경로 설정
+	mybatisMapper.createMapper(['./models/' + param.mapper + '.xml']);
+	var time = new Date();
+	console.log('## ' + time + ' ##');
+	console.log('\n Called Mapper Name  = ' + param.mapper);
+
+	var format = { language: 'sql', indent: '  ' };
+	//mysql 쿼리 정보 세팅
+	var query = mybatisMapper.getStatement(param.mapper, param.mapper_id, param, format);
+	console.log('\n========= Node Mybatis Query Log Start =========');
+	console.log('* mapper namespce : ' + param.mapper + '.' + param.mapper_id + ' *\n');
+	console.log(query + '\n');
 
 	connection.query(query, function (error, results) {
+		if (error) {
+			console.log('db error************* : ' + error);
+		}
+		var time2 = new Date();
+		console.log('## ' + time2 + ' ##');
+		console.log('## RESULT DATA LIST ## : \n', results);
 		string = JSON.stringify(results);
 		var json = JSON.parse(string);
 		res.send({ json });
